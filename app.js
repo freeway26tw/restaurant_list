ca6d66f05283330f2f830ba7d3d43ef388fd1642
freeway26tw <freeway26tw@gmail.com>
2022-12-07 23:04:23 +0800

    let restaurant category search available

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+    return (restaurant.name + restaurant.category).toLowerCase().includes(keyword.toLowerCase())
   })
   res.render('index', { restaurants: restaurants, keyword: keyword })
 })
