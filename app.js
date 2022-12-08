4ee0bc5b9ddcef0e6b065ff0c68e09ca51dc48ba
freeway26tw <freeway26tw@gmail.com>
2022-12-08 12:26:48 +0800

    shorten restaurantList.results in route

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const restaurantList = require('./restaurant.json')
+const restaurantList = require('./restaurant.json').results
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -12,7 +12,7 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
+  res.render('index', { restaurants: restaurantList })
 })
 
 app.get('/search', (req, res) => {
@@ -20,7 +20,7 @@ app.get('/search', (req, res) => {
   // if no keyword, redirect to baseUrl
   if (!keyword) return res.redirect(req.baseUrl + '/')
 
-  const restaurants = restaurantList.results.filter(restaurant => {
+  const restaurants = restaurantList.filter(restaurant => {
     return (restaurant.name + restaurant.category).toLowerCase().includes(keyword.toLowerCase())
   })
   // check if results found
@@ -28,14 +28,14 @@ app.get('/search', (req, res) => {
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(
+  const restaurant = restaurantList.find(
     restaurant => restaurant.id.toString() === req.params.restaurant_id
   )
   res.render('show', { restaurant })
 })
 
 app.get('/restaurants/explore/:restaurant_category', (req, res) => {
-  const restaurants = restaurantList.results.filter(restaurant => {
+  const restaurants = restaurantList.filter(restaurant => {
     return restaurant.category.toString() === req.params.restaurant_category
   })
   res.render('index', { restaurants })
