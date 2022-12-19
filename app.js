15caec1bd7c587f871bab8c940d1e3abb6a8c1d5
freeway26tw <freeway26tw@gmail.com>
2022-12-19 12:01:34 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json').results
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // setting template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
---
+++ models/restaurant-list.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const restaurantListSchema = new Schema({
+  id: {
+    type: Integer,
+    required: true
+  },
+  name: {
+    type: String
+  },
+  name_en: {
+    type: String
+  },
+  category: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  phone: {
+    type: String
+  },
+  google_map: {
+    type: String
+  },
+  rating: {
+    type: Double
+  },
+  description: {
+    type: String
+  }
+})
+module.exports = mongoose.model('Restaurant-list', restaurantListSchema)
