35accb94a48bc81fe666160ef01d4aea36d1a087
freeway26tw <freeway26tw@gmail.com>
2022-12-22 12:52:22 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const methodOverride = require('method-override')
 const Restaurant = require("./models/Restaurant")
 
 const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
 const port = 3000
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
--- models/seeds/restaurantListSeeder.js
+++ models/seeds/restaurantListSeeder.js
@@ -1,17 +1,12 @@
-const mongoose = require('mongoose')
 const Restaurant = require('../Restaurant.js')
 const restaurant = require('../../restaurant.json').results
+const db = require('../../config/mongoose')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
-  console.log('mongodb connected!')
   Restaurant.create(restaurant)
+  console.log('Done!')
 })
