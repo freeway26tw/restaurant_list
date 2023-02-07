520040cbbdcaebb0ff686369b499ab5d557faba1
freeway26tw <freeway26tw@gmail.com>
2023-02-07 08:25:56 +0800

    modify restaurantSeeder

--- models/seeds/restaurantListSeeder.js
+++ models/seeds/restaurantListSeeder.js
@@ -1,12 +1,48 @@
+const bcrypt = require('bcryptjs')
 const Restaurant = require('../Restaurant.js')
+const User = require('../user')
 const restaurant = require('../../restaurant.json').results
 const db = require('../../config/mongoose')
+
+const SEED_USERS = [
+  {
+    email: 'user1@example.com',
+    password: '12345678',
+    restaurant: [1, 2, 3]
+  },
+  {
+    email: 'user2@example.com',
+    password: '12345678',
+    restaurant: [4, 5, 6]
+  }
+]
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 db.once('open', () => {
-  Restaurant.create(restaurant)
-  console.log('Done!')
+  Promise.all(Array.from(SEED_USERS, user => {
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(user.password, salt))
+      .then(hash =>
+        User.create({
+          name: user.name,
+          email: user.email,
+          password: hash
+        }))
+      .then(userCreated => {
+        const restaurantFiltered = restaurant.filter(item => user.restaurant.includes(item.id))
+        return Promise.all(Array.from(
+          { length: restaurantFiltered.length },
+          (_, i) => Restaurant.create({ ...restaurantFiltered[i], userId: userCreated._id })
+        ))
+      })
+
+  }))
+    .then(() => {
+      console.log('Seed data created!')
+      process.exit()
+    })
 })
--- models/user.js
+++ models/user.js
@@ -6,15 +6,12 @@ const userSchema = new Schema({
   },
   email: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   password: {
     type: String,
     required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
   }
 })
 module.exports = mongoose.model('User', userSchema)
