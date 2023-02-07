5dc44cf22386696fde1cec78e79812166dec287b
freeway26tw <freeway26tw@gmail.com>
2023-02-07 12:45:09 +0800

    feat: modify password toggle and registration validator

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,3 +2,17 @@ function order() {
   const sort = document.querySelector('#sort')
   console.log(sort.value)
 }
+
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelectorAll("#password, #confirmPassword");
+
+togglePassword.addEventListener("click", function () {
+  // toggle the type attribute
+  password.forEach(p => {
+    const type = p.getAttribute("type") === "password" ? "text" : "password";
+    p.setAttribute("type", type);
+    // toggle the icon
+    this.classList.toggle("bi-eye")
+  })
+
+})
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
+const { check, validationResult } = require('express-validator')
 const User = require('../../models/user')
 const router = express.Router()
 router.get('/login', (req, res) => {
@@ -15,50 +16,54 @@ router.post('/login', passport.authenticate('local', {
 router.get('/register', (req, res) => {
   res.render('register')
 })
-router.post('/register', (req, res) => {
-  const { name, email, password, confirmPassword } = req.body
-  const errors = []
-  if (!name || !email || !password || !confirmPassword) {
-    errors.push({ message: '所有欄位都是必填。' })
-  }
-  if (password !== confirmPassword) {
-    errors.push({ message: '密碼與確認密碼不相符！' })
-  }
-  if (errors.length) {
-    return res.render('register', {
-      errors,
-      name,
-      email,
-      password,
-      confirmPassword
-    })
-  }
-  User.findOne({ email }).then(user => {
-    if (user) {
-      errors.push({ message: '這個 Email 已經註冊過了。' })
-      res.render('register', {
-        errors,
+router.post('/register', [
+  check('email').trim().notEmpty().withMessage('Please enter your email').isEmail().withMessage('Please enter valid email'),
+  check('password').trim().isLength({min: 8}).withMessage('Password less than 8 characters').isString().withMessage('Please enter valid password'),
+  check('confirmPassword').custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('Password and confirm password are not match!')
+    }
+    return true
+  })
+],
+  (req, res) => {
+    const { name, email, password, confirmPassword } = req.body
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+      return res.status(422).render('register', {
+        errorMessages: errors.array(),
         name,
         email,
         password,
         confirmPassword
       })
     }
-    return bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(password, salt))
-      .then(hash => User.create({
-        name,
-        email,
-        password: hash
-      }))
-      .then(() => res.redirect('/'))
-      .catch(error => console.log(error))
+    User.findOne({ email }).then(user => {
+      if (user) {
+        errors.push({ message: 'This email had been registered' })
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      }
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({
+          name,
+          email,
+          password: hash
+        }))
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+    })
   })
-})
 router.get('/logout', (req, res) => {
   req.logout()
-  req.flash('success_msg', '你已經成功登出。')
+  req.flash('success_msg', 'You have signed up successfully')
   res.redirect('/users/login')
 })
 
